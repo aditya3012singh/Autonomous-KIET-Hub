1982697806a2b0c2d66804d591d91e999a9932b7
Aditya <aditya.2428cseai@gmail.com>
2025-06-29 17:39:38 +0530

    Just make minor change in api.ts

--- UserPanel/src/services/api.ts
+++ UserPanel/src/services/api.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:3000/api/v1';
-import { toast } from 'react-toastify';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 let forceLogoutFn: () => void = () => {};
 
